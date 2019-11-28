3e2f50bcd79eb79807823596a08e013186ea28c8
bolo <bolosig@gmail.com>
2019-11-28 15:07:07 +0100

    componentes basicos para la aplicación

--- src/app.js
+++ src/app.js
@@ -1,44 +1,23 @@
 
-import './css/app.css'
-import mapboxgl from "mapbox-gl";
-import StylesControl from 'mapbox-gl-controls/lib/styles';
-
+import createHeader from "./components/header";
+import createRoot from "./components/root";
+import createFooter from "./components/footer";
+import createMap from "./components/map";
+import createSidebar from "./components/sidebar";
+import "./css/app.css";
 
 export default () => {
 
-  console.log('Init app...')
+  console.log("Init app...");
 
-  const map = new mapboxgl.Map({
-    container: "map",
-    minZoom: 2,
-    maxZoom: 16,
-    hash: true,
-    style: "https://tilemaps.icgc.cat/tileserver/styles/orto-inun-rc.json",
-    center: [0.681, 40.7102],
-    zoom: 11.18,
-    attributionControl: false
-  });
+  document.getElementById("header").innerHTML = createHeader();
 
+  document.getElementById("root").innerHTML = createRoot();
 
+  document.getElementById("footer").innerHTML = createFooter();
 
+  createMap();
 
-  map.addControl(new StylesControl({
-    styles: [
-      {
-        label: 'Orto',
-        styleName: 'Mapbox Streets',
-        styleUrl: 'https://tilemaps.icgc.cat/tileserver/styles/orto-inun-rc.json',
-      }, {
-        label: 'Topo',
-        styleName: 'totpo',
-        styleUrl: 'https://tilemaps.icgc.cat/tileserver/styles/topo-inun-rc.json',
-      },
-    ],
-    onChange: (style) => console.log(style),
-    // map.setStyle(style.styleUrl);
-
-  }), 'top-right');
-
-
+  createSidebar(document.getElementById("root"));
 
 }
